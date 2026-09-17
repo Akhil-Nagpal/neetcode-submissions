28014ab76bb42db1a3d1bde50debabfbb6721d82
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-09-17 16:39:32 +0530

    Add: anagram-groups - submission-6

---
+++ Data Structures & Algorithms/anagram-groups/submission-6.js
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        // brute force using hashmap not quite actual brute force but there is another way 
+
+        let map = new Map();
+
+        // traverse instill the end of the array 
+        for (let i = 0; i < strs.length; i++) {
+            // sort the string
+            let sortedStr = strs[i].split("").sort().join();
+
+            // check if the key exists in map or not, if not then add the key
+            if (!map.has(sortedStr)) {
+                map.set(sortedStr, [])
+            }
+            // if key exists, then get the value of that key and add new value
+            map.get(sortedStr).push(strs[i]);
+        }
+        return Array.from(map.values());
+        
+    }
+}
