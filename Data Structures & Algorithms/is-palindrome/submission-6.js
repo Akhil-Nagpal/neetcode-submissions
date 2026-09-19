202526bc05a73a02d1122fe23d4ac95ab3ef9053
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-09-19 17:57:04 +0530

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s) {
+        // optimised code using two pointers
+        // approach - unlike brute force here we use opposite pointers to track and check if the pointers have same character or not 
+        // 1 - initiate pointers
+        // 2 - loop ntill left is smaller then right 
+        // 3 - check if the pointer is non alphanumeric character if yes make pointers move 
+        // if it doesn't match return false 
+    }
+}
