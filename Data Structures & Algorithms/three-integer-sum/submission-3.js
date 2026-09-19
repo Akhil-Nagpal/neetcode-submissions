8b8d8f5fa4dd9dbd08e25cb1ec01870ba7fc662f
! Ak ! <96314234+Akhil-Nagpal@users.noreply.github.com>
2026-09-19 21:43:06 +0530

    Add: three-integer-sum - submission-3

---
+++ Data Structures & Algorithms/three-integer-sum/submission-3.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        // optimised code using two pinters 
+        // approach - 
+        // inorder to work with this we need to sort the array first
+        // 1 - I will fix the pointer using loop 
+        // 2 - now check if the ith value is duplicate or not, 
+        // 3 - if not, then run another loop to calculate the sum like two sum II
+        // 4 - check if the left is duplicate then move left forward, smae goes for right but the other way 
+        // 5 - check if the sum is smaller move left forward 
+        // 6 - check if the sum is larger then move right backward  
+
+
+    }
+}
